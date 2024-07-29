b5c6225c20d25535949e8201d3c344b9e23f58ca
Eryck Santos <dev.erycksantos@gmail.com>
2024-07-29 19:43:31 -0300

    Atualizando endpoints para usar o banco de dados real

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,20 +2,34 @@ import pytest  # type: ignore
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
+from sqlalchemy.pool import StaticPool
 
 from fast_zero.app import app
-from fast_zero.models import table_registry
+from fast_zero.database import get_session
+from fast_zero.models import User, table_registry
 
 
 @pytest.fixture()
-def client():
-    return TestClient(app)
+def client(session):
+    def get_session_override():
+        return session
+
+    with TestClient(app) as client:
+        app.dependency_overrides[get_session] = get_session_override
+
+        yield client
+
+    app.dependency_overrides.clear()
 
 
 @pytest.fixture()
 def session():
     # cria um banco de dados volátil em memória
-    engine = create_engine('sqlite:///:memory:')
+    engine = create_engine(
+        'sqlite:///:memory:',
+        connect_args={'check_same_thread': False},
+        poolclass=StaticPool,
+    )
 
     # cria os metadados
     table_registry.metadata.create_all(engine)
@@ -28,3 +42,14 @@ def session():
     # por fim, após o resultado do  teste
     # faça um tier donw: desfaça toda a operação
     table_registry.metadata.drop_all(engine)
+
+
+@pytest.fixture()
+def user(session):
+    user = User(username='Teste', email='teste@test.com', password='testtest')
+
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+
+    return user
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from fast_zero.schemas import UserPublic
+
 
 def test_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')  # Act (ação) -> literalmente o teste
@@ -41,31 +43,19 @@ def test_crate_user(client):
 def test_read_users(client):
     response = client.get('/users/')
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'users': [
-            {
-                'username': 'eryck',
-                'email': 'eryck@test.com',
-                'id': 1,
-            }
-        ]
-    }
+    assert response.json() == {'users': []}
 
 
-def test_read_user_by_id(client):
-    response = client.get('/users/1')
-    response_error = client.get('/users/2')
+def test_read_users_with_user(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+
+    response = client.get('/users/')
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'username': 'eryck',
-        'email': 'eryck@test.com',
-        'id': 1,
-    }
-    assert response_error.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'users': [user_schema]}
 
 
-def test_update_user(client):
+def test_update_user(client, user):
     response = client.put(
         '/users/1',
         json={
@@ -94,11 +84,24 @@ def test_update_user(client):
     assert response_error.status_code == HTTPStatus.NOT_FOUND
 
 
-def test_delete_user(client):
+def test_delete_user(client, user):
     response = client.delete('/users/1')
     response_error = client.delete('/users/2')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'User deleted'}
 
+    assert response_error.status_code == HTTPStatus.NOT_FOUND
+
+
+def test_read_user_by_id(client):
+    response = client.get('/users/1')
+    response_error = client.get('/users/2')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'eryck',
+        'email': 'eryck@test.com',
+        'id': 1,
+    }
     assert response_error.status_code == HTTPStatus.NOT_FOUND
